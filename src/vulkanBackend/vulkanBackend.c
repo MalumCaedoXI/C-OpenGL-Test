f113384f54a02f13ce1840eb2c60c2a4c12da25d
MalumCaedoXI <MalumCaedoXI@proton.me>
2024-02-13 16:08:46 +0100

    Got logical device creation working!

--- src/vulkanBackend/vulkanBackend.c
+++ src/vulkanBackend/vulkanBackend.c
@@ -6,6 +6,8 @@
 #define height 600
 
 VkInstance instance;
+VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+VkDevice logicalDevice = VK_NULL_HANDLE;
 
 int checkValidationLayerAvailabillity(char** requiredValidationLayers, char validationLayerCount)
 {
@@ -93,6 +95,128 @@ int createInstance()
     return 0;
 }
 
+QueueFamilyIndices findQueueFamilies(VkPhysicalDevice targetDevice)
+{
+    QueueFamilyIndices indicies = {1};
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(targetDevice, &queueFamilyCount, NULL);
+
+    VkQueueFamilyProperties queueFamilies[queueFamilyCount];
+    vkGetPhysicalDeviceQueueFamilyProperties(targetDevice, &queueFamilyCount, queueFamilies);
+    char result = 0;
+    for (int i = 0; i < queueFamilyCount; i++)
+    {
+        if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            result = 1;
+            indicies.graphicsFamily = i;
+        }
+    }
+
+    indicies.valid &= result;
+
+    return indicies;
+}
+
+int physicalDeviceSuitable(VkPhysicalDevice targetDevice)
+{
+    VkPhysicalDeviceFeatures features;
+    vkGetPhysicalDeviceFeatures(targetDevice, &features);
+    VkPhysicalDeviceProperties properties;
+    vkGetPhysicalDeviceProperties(targetDevice, &properties);
+
+
+    if((properties.deviceType== VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU || properties.deviceType== VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU) && features.geometryShader && 
+    findQueueFamilies(targetDevice).valid)
+    {
+        printf("Found suitable physical device: %s!\n", properties.deviceName);
+        return 1;
+    }
+    return 0;
+}
+
+int pickPhysicalDevice()
+{
+    printf("Starting to check for physical devices!\n");
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, NULL);
+
+    if (deviceCount == 0)
+    {
+        printf("No physical devices were found during vulkan init!\n");
+        return 0;
+    }
+
+    VkPhysicalDevice devices[deviceCount] ;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices);
+
+    for (int i = 0; i < deviceCount; i++)
+    {
+        if (physicalDeviceSuitable(devices[i]))
+        {
+            physicalDevice = devices[i];
+            break;
+        }
+    }
+
+    if (physicalDevice == VK_NULL_HANDLE)
+    {
+        printf("No physical device was found to be suitable during vulkan init!\n");
+        return 0;
+    }
+    
+    return 1;
+}
+
+int createLogicalDevice()
+{
+    printf("Starting creation of logical device!\n");
+    QueueFamilyIndices indecies = findQueueFamilies(physicalDevice);
+    float queuePriority = 1.0f;
+
+    VkDeviceQueueCreateInfo queueCreateInfo = {};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = indecies.graphicsFamily;
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+
+    VkPhysicalDeviceFeatures deviceFeatures ={};
+
+    VkDeviceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+    createInfo.pQueueCreateInfos = &queueCreateInfo;
+    createInfo.queueCreateInfoCount = 1;
+
+    createInfo.pEnabledFeatures = &deviceFeatures;
+    createInfo.enabledLayerCount = 0;
+
+    char validationLayerCount = 1;
+    char validationEnabled = 0;
+
+    #ifdef DEBUG
+    validationEnabled = 1;
+    #endif
+
+    char* requiredValidationLayers[validationLayerCount];
+    requiredValidationLayers[0] = "VK_LAYER_KHRONOS_validation";
+    if (validationEnabled)
+    {
+        createInfo.enabledLayerCount = validationLayerCount;
+        createInfo.ppEnabledLayerNames = requiredValidationLayers;
+    }
+
+    if (vkCreateDevice(physicalDevice, &createInfo, NULL, &logicalDevice) != VK_SUCCESS) 
+    {
+        printf("Logical device creation failed!\n");
+        return 0;
+    }
+
+    printf("Logical device creation OK!\n");
+    return 1;
+}
+
+
 int initVulkan()
 {
     if (!createInstance())
@@ -108,8 +232,15 @@ int initVulkan()
     printf("Extentions are:\n");
     for (int i = 0; i < extensionCount; i++)
     {
-        printf(extensions[i].extensionName);
-        printf("\n");
+        printf("%s\n", extensions[i].extensionName);
+    }
+    if (!pickPhysicalDevice())
+    {
+        return 0;
+    }
+    if (!createLogicalDevice())
+    {
+        return 0;   
     }
 
     return 1;
--- src/vulkanBackend/vulkanBackend.h
+++ src/vulkanBackend/vulkanBackend.h
@@ -1,6 +1,11 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+typedef struct QueueFamilyIndices {
+    char valid;
+    uint32_t graphicsFamily; 
+} QueueFamilyIndices;
+
 int initVulkan();
 int initWindow(GLFWwindow** window);
 int cleanupWindow(GLFWwindow** window);
