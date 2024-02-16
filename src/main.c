b0c3360ea07b63e5f751da12c203a3e3755be75b
MalumCaedoXI <MalumCaedoXI@proton.me>
2024-02-16 18:34:31 +0100

    Dynamic rendering working, but complaining

--- src/main.c
+++ src/main.c
@@ -7,13 +7,15 @@
 int main() {
     GLFWwindow** window; 
     initWindow(window);
-    initVulkan(*window);
+    if(initVulkan(*window))
+    {
 
-    while (!glfwWindowShouldClose(*window)) {
-        glfwPollEvents();
-        drawFrame();
+        while (!glfwWindowShouldClose(*window)) {
+            glfwPollEvents();
+            drawFrame();
+        }
     }
-
     waitIdle();
     cleanupWindow(window);
+
 }
