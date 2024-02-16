7904b2d7673519eeb1af1b0539abdc8d94d404ac
MalumCaedoXI <MalumCaedoXI@proton.me>
2024-02-16 21:39:54 +0100

    Allow for GDB commits

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ glslc ./resources/shaders/baseVertexShader.vert -o ./build/baseVertexShader.spv
 glslc ./resources/shaders/baseFragmentShader.frag -o ./build/baseFragmentShader.spv
 
 cmake -S . -B ./build
-cmake --build ./build
+cmake -DCMAKE_BUILD_TYPE=Debug --build ./build
 
 ./test.sh
