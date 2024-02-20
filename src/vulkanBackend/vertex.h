b1982ab80a71be6ada9273f141c83a498f030e8c
MalumCaedoXI <MalumCaedoXI@proton.me>
2024-02-20 02:09:27 +0100

    Almost Working Now

--- src/vulkanBackend/vertex.h
+++ src/vulkanBackend/vertex.h
@@ -1,5 +1,6 @@
 #include <vulkan/vulkan.h>
 #include <math.h>
+#include <string.h>
 
 typedef float mat4[4][4];
 
@@ -30,3 +31,6 @@ VkVertexInputBindingDescription vertexGetBindingDescription();
 VkVertexInputAttributeDescription vertexGetPositionAttributeDescrition();
 VkVertexInputAttributeDescription vertexGetColorAttributeDescrition();
 void rotateMatrix(float inMatrix[4][4], float angle, float vector[3]);
+void lookAt(float mat[4][4], float eye[3], float center[3], float up[3]);
+void perspective(float matrix[4][4], float fovDegrees, float aspect, float near, float far);
+void oldRotateMatrix(float mat[4][4], char axis, float angle);
